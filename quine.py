7fb0de8749a20efb75bf157af6bcf2f24cdf98cd
tolvanea <alpi.tolvanen@tutanota.com>
2021-11-10 17:07:19 +0200

    Wrong repo whoops

--- quine.py
+++
@@ -1,4 +0,0 @@
-n, q = chr(10), chr(34)  # newline, quote
-l1 = "n, q = chr(10), chr(34)  # newline, quote"
-l4 = "print(l1, 'l1 = ' + q + l1, 'l4 = ', q + l4, l4, sep=q+n)"
-print(l1, 'l1 = ' + q + l1, 'l4 = '+ q + l4, l4, sep=q+n)
